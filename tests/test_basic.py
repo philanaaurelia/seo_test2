5f7b5cc6e995fdad9f1130cdec0eac1de4e2d522
Phil B <philanab@gmail.com>
2024-07-04 22:44:52 -0400

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 import unittest, sys
 
-sys.path.append('../') # imports python file from parent directory
+sys.path.append('..') # imports python file from parent directory
 from app import app #imports flask app object
 
 class BasicTests(unittest.TestCase):
